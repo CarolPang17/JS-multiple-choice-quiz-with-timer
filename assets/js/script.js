71732cdb1330207bedef1766b6ee4e6c4eee0737
carolpang <carolpang17@gmail.com>
2022-01-19 18:49:17 -0800

    some css added for answer choices and basic function structure added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,53 @@
-var questions = [
+const questions = [
   {
     question: "Commonly used data types DO Not Include:",
-    choices: ["1.string", "2.booleans", "3.alerts", "numbers"],
-    correctAnswer: 3,
+    answers: [
+      { text: "1.string", correct: false },
+      { text: "2.booleans", correct: false },
+      { text: "3.alerts", correct: true },
+      { text: "4.numbers", correct: false }
+    ],
   },
   {
     question:
       "The condition in an if / else statement is enclosed with ______.",
-    choices: [
-      "1. quotes",
-      "2. curly brackets",
-      "3. parenthesis",
-      "4. square brackets",
-    ],
-    correctAnswer: 3,
+    answers: [
+      { text: "1. quotes", correct: false },
+      { text: "2. curly brackets", correct: false },
+      { text: "3. parenthesis", correct: true },
+      { text: "4. square brackets", correct: false }
+    ]
   },
   {
     question: "Arrays in JavaScript can be used to store _____.",
-    choices: [
-      "1.numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      "4. all of the above",
-    ],
-    correctAnswer: 4,
+    answers: [
+      { text:  "1.numbers and strings", correct: false },
+      { text:  "2. other arrays", correct: false },
+      { text: "3. booleans", correct: false },
+      { text: "4. all of the above", correct: true }
+    ]
   },
   {
     question:
       "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-    correctAnswer: 3,
+    answers: [
+      { text:  "1. commas", correct: false },
+      { text:  "2. curly brackets", correct: false },
+      { text: "3. quotes", correct: true },
+      { text: "4. parenthesis", correct: false }
+    ]
   },
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    choices: [
-      "1. JavaScript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console.log",
-    ],
-    correctAnswer: 4,
-  },
+    answers: [
+      { text: "1. JavaScript", correct: false },
+      { text: "2. terminal/bash", correct: false },
+      { text: "3. for loops", correct: true },
+      { text: "4. console.log", correct: false }
+    ]
+  }
 ];
-
 
 var timer = 75;
 var interval = 1000;
@@ -61,4 +65,18 @@ $("#start").click(function () {
   displayCurrentQuestion();
 });
 
+const startButton = document.getElementsById("start-btn");
+const questionContainerElement = document.getElementsById("question-container");
 
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+  console.log("Started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {}
+
+function selectAnswer() {}
